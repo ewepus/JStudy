3156fbfe63974245a4a203c5ee934d413fbe999f
ewepus <billshifr51@gmail.com>
2025-03-13 23:25:29 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,14 @@
+let bum = 42;
+let name = 'ewepu';
+const isBobby = true;
+let sdf = bum.toExponential();
+
+//alert(name)
+console.log(bum, name, isBobby);
+console.log(sdf);
+
+const resultElement = document.getElementById('increaseByOne');
+console.log(resultElement);
+console.log(resultElement.textContent);
+resultElement.textContent = 42;
+console.log(resultElement.textContent)
