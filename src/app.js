9871111526dc395d4375f72611140418370f1dd6
ewepus <billshifr51@gmail.com>
2025-03-16 18:49:35 +0300

    calculator v.2

--- src/app.js
+++ src/app.js
@@ -19,20 +19,74 @@ const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
 const plusButton = document.getElementById('plus');
 const minusButton = document.getElementById('minus');
+const multiplyButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
+
 const submitButton = document.getElementById('submit');
 
-let isPlus = true;
+let operation = '+';
 
 plusButton.onclick = function () {
-  isPlus = true;
-  console.log('isPlus:', isPlus);
+  operation = '+';
+  console.log('operation:', operation);
 };
 minusButton.onclick = function () {
-  isPlus = false;
-  console.log('isPlus:', isPlus);
+  operation = '-';
+  console.log('operation:', operation);
 };
+multiplyButton.onclick = function () {
+  operation = '*';
+  console.log('operation:', operation);
+};
+divideButton.onclick = function () {
+  operation = '/';
+  console.log('operation:', operation);
+};
+
 submitButton.onclick = function () {
-  if (isPlus)
-    resultElement.textContent = Number(input1.value) + Number(input2.value);
-  else resultElement.textContent = Number(input1.value) - Number(input2.value);
+  switch (operation) {
+    case '+':
+      printResult();
+      break;
+    case '-':
+      printResult();
+      break;
+    case '*':
+      printResult();
+      break;
+    case '/':
+      printResult();
+      break;
+    default:
+      console.log('invalid input');
+  }
 };
+
+function printResult() {
+  const sum = computeAction(operation);
+  resultColour(sum);
+  resultElement.textContent = sum;
+}
+
+function resultColour(result) {
+  if (result < 0) {
+    resultElement.style.color = 'red';
+  } else if (result > 0) resultElement.style.color = 'blue';
+  else resultElement.style.color = 'green';
+}
+
+function computeAction(operation) {
+  let sum;
+  switch (operation) {
+    case '+':
+      return (sum = Number(input1.value) + Number(input2.value));
+    case '-':
+      return (sum = Number(input1.value) - Number(input2.value));
+    case '*':
+      return (sum = Number(input1.value) * Number(input2.value));
+    case '/':
+      return (sum = Number(input1.value) / Number(input2.value));
+    default:
+      console.log('unknown operation');
+  }
+}
